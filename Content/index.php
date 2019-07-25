bd2d78baedff8afe4f94478bb759af98a240e742
ejgordillo <elias0232@gmail.com>
2019-07-25 09:37:39 -0600

    fixes something

---
+++ Content/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = "<h1>Elias</h1>";
+
+echo $name;
+
+$string1 = "<p>This is the first part";
+$string2 = "of a thing I say.</p>"; 
+
+echo $string1." ".$string2;
+
+$myNumber = 46;
+
+$calculation = $myNumber * 31 / 97 + 4;
+
+echo "<p>The result of my calculation is ".$calculation."</p>";
+
+$myBool = true; 
+
+echo "$myBool";
+
+?>
